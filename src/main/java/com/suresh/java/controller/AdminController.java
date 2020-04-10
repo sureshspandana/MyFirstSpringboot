47b9f864121d91f9309e5e4ed6b7f2044b898f0a
sureshspandana <sureshc.behera@gmail.com>
2020-04-10 15:25:34 +0530

    Commit of MyFirst Springboot application

---
+++ src/main/java/com/suresh/java/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.suresh.java.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+
+	@GetMapping("/admin")
+	public String getAdminDetails() {
+		return "Admin Details from DB";
+	}
+}
