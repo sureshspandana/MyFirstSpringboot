f78dffef5d86fcbfe5ecedaa30da82452a9e7817
sureshspandana <sureshc.behera@gmail.com>
2020-04-10 15:23:12 +0530

    Commit of MyFirst Springboot application

--- src/main/java/com/suresh/java/MyFirstSpringbootApplication.java
+++ src/main/java/com/suresh/java/MyFirstSpringbootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyFirstSpringbootApplication {
 
 	public static void main(String[] args) {
+		System.out.println("MyFirstSpringbootApplication>>>>>>>");
 		SpringApplication.run(MyFirstSpringbootApplication.class, args);
 	}
 
